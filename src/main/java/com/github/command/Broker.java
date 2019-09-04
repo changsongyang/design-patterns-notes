d0444d90bf3c318b0c6a76c4f280a3329949fed6
qinxuewu <870439570@qq.com>
2019-09-04 20:36:12 +0800

    命令模式

---
+++ src/main/java/com/github/command/Broker.java
@@ -0,0 +1,28 @@
+package com.github.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建命令调用类
+ *
+ * @author qinxuewu
+ * @create 19/9/4下午7:43
+ * @since 1.0.0
+ */
+
+
+public class Broker {
+    private List<Order> orderList = new ArrayList<Order>();
+
+    public void takeOrder(Order order){
+        orderList.add(order);
+    }
+
+    public void placeOrders(){
+        for (Order order : orderList) {
+            order.execute();
+        }
+        orderList.clear();
+    }
+}
