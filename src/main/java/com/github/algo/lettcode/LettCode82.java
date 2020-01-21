a2ae1a9cf49cd996d9f55b63df25f931b803b7b6
codeing <870439570@qq.com>
2020-01-21 16:15:17 +0800

    链表专题练习

--- src/main/java/com/github/algo/lettcode/LettCode82.java
+++ src/main/java/com/github/algo/lettcode/LettCode82.java
@@ -20,10 +20,35 @@ public class LettCode82 {
              ListNode(int x) { val = x; }
    }
 
+    /**
+     * 删除链表中所有重复的元素 ，这题和删除链表重复的元素 只保存一个的变体
+     *   需要一个虚拟头节点 和一个前指针辅助per，
+     *   外层循环不断的遍历  内层循环比遍历判断cur.val=cur.next.val 相等移动cur节点的位置
+     *   当per.next.next=cur 时 说明 cur只移动过一次 则 per=per.next 移动一位
+     *   如果不相等则说明移动了多次 直接per.next=cur 即可 中间的元素就全删除了
+     *  两重循环  当
+     * @param head
+     * @return
+     */
     public  static ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode per=dummy;
+        ListNode cur=head;
+        while (cur!=null){
+            while (cur.next!=null && cur.val==cur.next.val){
+                cur=cur.next;
+            }
+            cur=cur.next;
 
+            if(per.next.next==cur){
+                per=per.next;
+            }else{
+                per.next=cur;
+            }
+        }
 
-        return  null;
+        return  dummy.next;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/github/algo/lettcode/LettCode86.java
@@ -0,0 +1,36 @@
+package com.github.algo.lettcode;
+
+
+/**
+ 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+
+ 你应当保留两个分区中每个节点的初始相对位置。
+
+ 示例:
+
+ 输入: head = 1->4->3->2->5->2, x = 3
+ 输出: 1->2->2->4->3->5
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/partition-list
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LettCode86 {
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     *  利用两个辅助节点low high
+     *  遍历head链表  小于x 放入low链表中  大于等于x 放入high 链表中
+     *  最后low.next=high  合并成一个链表就over了
+     * @param head
+     * @param x
+     * @return
+     */
+    public ListNode partition(ListNode head, int x) {
+
+        return  null;
+    }
+}
