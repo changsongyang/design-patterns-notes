a7a9f78dc164a66e506ffd124cfb75b5ed0ec617
codeing <870439570@qq.com>
2020-03-06 11:08:31 +0800

    数据结构与算法

---
+++ src/main/java/com/github/algo/heap/BinaryHeap.java
@@ -0,0 +1,35 @@
+package com.github.algo.heap;
+
+
+/**
+ * 功能描述:  二叉堆（最大堆）
+ * @author: qinxuewu
+ * @date: 2020/3/6 11:06
+ * @since 1.0.0
+ */
+public class BinaryHeap<E> extends  AbstractHeap<E> {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public E get() {
+        return null;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E replace(E element) {
+        return null;
+    }
+}
