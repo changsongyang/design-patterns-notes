14c8e2d50f867509f1da01dadc806e512d0956ca
codeing <870439570@qq.com>
2020-03-09 14:45:48 +0800

    二叉树

--- src/main/java/com/github/algo/tree/BinaryTree.java
+++ src/main/java/com/github/algo/tree/BinaryTree.java
@@ -2,6 +2,8 @@ package com.github.algo.tree;
 
 
 import com.github.algo.TreeNode;
+import com.github.algo.stack.Stack;
+import lombok.val;
 
 /**
  * 功能描述: 二叉树的 前序遍历 中序遍历 后续遍历
@@ -33,12 +35,17 @@ public class BinaryTree {
         righr1.setRight(righr3);
 
 
-        System.out.println("前序遍历............ABDECFG.....");
-//        preOrder(root);    // ABDECFG
-        System.out.println(preOrder(root,3).toString());
-//        System.out.println("中序遍历...........DBEAFCG......");
-//        inorderOrder(root);
-//
+//        System.out.println("前序遍历............ABDECFG.....");
+////        preOrder(root);    // ABDECFG
+//        System.out.println(preOrder(root,3).toString());
+//        preOrder2(root);
+
+        System.out.println("中序遍历...........DBEAFCG......");
+        inorderOrder(root);
+        inorderOrder2(root);
+
+
+
 //        System.out.println("后序遍历..........DEBFGCA.......");
 //        postOrder(root);
 
@@ -111,6 +118,72 @@ public class BinaryTree {
         postOrder(root.getLeft());
         postOrder(root.getRight());
         System.out.println(root.toString());
+    }
+
+
+    // 非递归前序遍历
+
+    /**
+     * 非递归前序遍历  根节点>左节点>右节点
+     * 利用栈实现，首先遍历当前节点，然后判断当前节点的right子树是否为空，不为空则入栈
+     * 然后记录遍历往左边遍历，当栈为空空时 则直接f返回
+     * 不为空则pop出栈赋值给当前node
+     * @param root
+     */
+    public static   void preOrder2(TreeNode root){
+        if(root==null){
+            return;
+        }
+        // 栈保存右子树
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode node=root;
+        while (true){
+            if(node!=null){
+                // 访问当前节点
+                System.out.println(node.getName());
+                // 右子树不为空 则入栈
+                if(node.getRight()!=null){
+                    stack.push(node.getRight());
+                }
+                // 向左走
+                node=node.getLeft();
+            }else if(stack.isEmpty()){
+                return;
+            }else {
+                node=stack.pop();
+            }
+        }
+    }
+
+
+    /**
+     * 非递归中序遍历 左节点>根节点》右节点  利用栈
+     * 如果当前node 不为空  则把左节点入栈 并且继续往左走
+     * 如果当然栈为null 直接返回
+     * 栈不为空    则出栈元素赋值为当前node,  然后访问此节点。并且当前节点向右移动，继续遍历当节右节点的左子树
+     * @param root
+     */
+    public static   void inorderOrder2(TreeNode root){
+        if(root==null){
+            return;
+        }
+        TreeNode node=root;
+        Stack<TreeNode> stack=new Stack<>();
+        while (true){
+            if(node!=null){
+                stack.push(node);
+                node=node.getLeft();
+            }else if(stack.isEmpty()){
+                return;
+            }else{
+                // 栈不为空
+                node=stack.pop();
+              // 访问node节点
+                System.out.println(node.getName());
+                // 往有节点进行中序遍历
+                node=node.getRight();
+            }
+        }
     }
 
 
